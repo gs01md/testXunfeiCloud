46ae5797db638e01685f874db0d5198cd522794b
gs01md <gs01md@163.com>
2016-04-27 00:07:39 +0800

    添加初始工程

---
+++ testXunfeiCloud/testXunfeiCloud/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testXunfeiCloud
+//
+//  Created by WoodGao on 16/4/27.
+//  Copyright © 2016年 wood. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testXunfeiCloud/testXunfeiCloud/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testXunfeiCloud
+//
+//  Created by WoodGao on 16/4/27.
+//  Copyright © 2016年 wood. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
