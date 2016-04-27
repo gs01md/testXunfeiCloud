0ac703ac67e9513f7aea973cd18f725f48e4947d
gs01md <gshzyfx@163.com>
2016-04-27 11:24:40 +0800

    初步识别

--- testXunfeiCloud/testXunfeiCloud/ViewController.m
+++ testXunfeiCloud/testXunfeiCloud/ViewController.m
@@ -8,7 +8,32 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+//带界面的语音识别控件
+
+#import "iflyMSC/IFlyRecognizerViewDelegate.h"
+
+#import "iflyMSC/IFlyRecognizerView.h"
+
+
+
+//不带界面的语音识别控件
+
+#import "iflyMSC/IFlySpeechRecognizerDelegate.h"
+
+#import "iflyMSC/IFlySpeechRecognizer.h"
+
+#import "iflyMSC/IFlySpeechUtility.h"
+
+#import "iflyMSC/IFlySpeechConstant.h"
+
+
+
+@interface ViewController ()<
+IFlyRecognizerViewDelegate
+>
+{
+    IFlyRecognizerView      *_iflyRecognizerView;
+}
 
 @end
 
@@ -16,12 +41,50 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+
+    [self startRecognizer];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void) startRecognizer {
+    
+    NSString *initString = [[NSString alloc] initWithFormat:@"appid=%@",@""];
+    [IFlySpeechUtility createUtility:initString];
+    
+    //初始化语音识别控件
+    _iflyRecognizerView = [[IFlyRecognizerView alloc] initWithCenter:self.view.center];
+    
+    [self.view addSubview:_iflyRecognizerView];
+    _iflyRecognizerView.delegate = self;
+    [_iflyRecognizerView setParameter: @"iat" forKey: [IFlySpeechConstant IFLY_DOMAIN]];
+    //asr_audio_path保存录音文件名，如不再需要，设置value为nil表示取消，默认目录是documents
+    [_iflyRecognizerView setParameter:nil forKey:[IFlySpeechConstant ASR_AUDIO_PATH]];
+    //启动识别服务
+    [_iflyRecognizerView start];
+}
+
+
+
+
+/*识别结果返回代理
+ @param resultArray 识别结果
+ @ param isLast 表示是否最后一次结果
+ */
+- (void)onResult: (NSArray *)resultArray isLast:(BOOL) isLast
+{
+    
+    NSLog(@"%@",resultArray);
+    
+}
+/*识别会话错误返回代理
+ @ param  error 错误码
+ */
+- (void)onError: (IFlySpeechError *) error
+{
+    
+    NSLog(@"%@",error);
+    
+    [self startRecognizer];
 }
 
 @end
